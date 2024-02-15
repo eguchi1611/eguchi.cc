10c5a0e310814f2917baaf4e1220c05682843975
Keita Ito <eguchi@eguchi.cc>
2024-02-15 12:24:43 +0000

    ヘッダーレイアウトの変更

--- src/app/(index)/_components/Header.tsx
+++ src/app/(index)/_components/Header.tsx
@@ -1,5 +1,11 @@
+import { IconMenu2 } from "@tabler/icons-react";
+
 export default function Header() {
   return (
-    <div className="h-full min-h-16 w-full min-w-32 bg-teal-200">header</div>
+    <div className="flex h-full min-h-16 w-full min-w-32 bg-teal-200">
+      <button className="flex w-16 items-center justify-center bg-red-200">
+        <IconMenu2 size={36} stroke={1} />
+      </button>
+    </div>
   );
 }
