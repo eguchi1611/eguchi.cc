dae8fccbfbf55d4935337838868e51d83bcd33f6
Keita Ito <eguchi@eguchi.cc>
2024-02-15 01:03:06 +0000

    Headerのレイアウトを実装

---
+++ src/app/(index)/_components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <div className="h-full min-h-16 w-full min-w-32 bg-teal-200">header</div>
+  );
+}
--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,8 +1,12 @@
+import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 
 export default function IndexPage() {
   return (
-    <div>
+    <div className="sm:pr-32">
+      <div className="fixed left-0 right-0 top-0 sm:bottom-0 sm:left-auto">
+        <Header />
+      </div>
       <Hero />
       <p className="text-red-500">Hello World</p>
       <p>Hello</p>
