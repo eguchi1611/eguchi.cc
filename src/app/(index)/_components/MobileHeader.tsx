727a5f3870f1444fad598194f5df6339d3b3070c
Keita Ito <eguchi@eguchi.cc>
2024-02-19 19:19:20 +0900

    ファイルの全削除

--- src/app/(index)/_components/MobileHeader.tsx
+++
@@ -1,9 +0,0 @@
-import MobileMenu from "./MobileMenu";
-
-export default function MobileHeader() {
-  return (
-    <div className="z-50 flex min-h-16 bg-teal-100 bg-opacity-50">
-      <MobileMenu />
-    </div>
-  );
-}
--- src/styles/iconButton.ts
+++
@@ -1,7 +0,0 @@
-import { tv } from "tailwind-variants";
-import { button } from "./button";
-
-export const iconButton = tv({
-  extend: button,
-  base: "flex items-center gap-2",
-});
