b49fde7dfcceae0e3ce4249981945ae823da6e2f
Keita Ito <eguchi@eguchi.cc>
2024-07-03 02:39:19 +0900

    feat(www): ヘッダーの内容を作成

--- apps/www/src/layouts/Header.tsx
+++ apps/www/src/layouts/Header.tsx
@@ -1,7 +1,26 @@
+import { IconHome, IconMessage, IconNotebook } from "@tabler/icons-react";
+import Link from "next/link";
+
+const items = [
+  { label: "Home", Icon: IconHome, href: "/" },
+  { label: "Articles", Icon: IconNotebook, href: "#" },
+  { label: "Contact", Icon: IconMessage, href: "#" },
+];
+
 export function Header() {
   return (
     <div className="bg-primary-100/50 backdrop-blur">
-      <div className="container px-4 py-2">HEADER</div>
+      <div className="container px-4 py-2">
+        <ul className="flex gap-2 p-2">
+          {items.map(({ label, Icon, href }) => (
+            <li key={label}>
+              <Link href={href} className="flex items-center gap-2">
+                <Icon />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
