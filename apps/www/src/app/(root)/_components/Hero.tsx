3c0b399c345578fa7fde625995810767c74e4468
Keita Ito <eguchi@eguchi.cc>
2024-07-04 02:28:46 +0900

    fix(www): heroを修正

--- apps/www/src/app/(root)/_components/Hero.tsx
+++ apps/www/src/app/(root)/_components/Hero.tsx
@@ -1,14 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./Hero.module.css";
+import { useOffsetTop } from "@/hooks/useOffsetTop";
+import { useRef } from "react";
+import { useWindowSize } from "@/hooks/useWindowSize";
 
 export function Hero() {
+  const masterRef = useRef(null);
+  const { viewportTop } = useOffsetTop(masterRef);
+  const { windowSize } = useWindowSize();
+
+  const a =
+    viewportTop && windowSize
+      ? Math.max(
+          0,
+          Math.min(
+            1,
+            1 - (viewportTop + windowSize?.height) / windowSize?.height,
+          ),
+        )
+      : 0;
+
+  console.log(a);
+
   return (
     <div
-      className={`grid min-h-svh place-content-center place-items-center ${styles.bg}`}
+      ref={masterRef}
+      className={`relative grid min-h-svh place-content-center place-items-center bg-primary-50 bg-fixed ${styles["bg-pattern"]}`}
     >
-      <Link href="/">
+      <div className="absolute inset-0 bg-primary-700" style={{ opacity: a }} />
+      <Link href="/" className="z-10">
         <Image
           alt=""
           src="/logo.svg"
--- apps/www/src/layouts/Layout.tsx
+++ apps/www/src/layouts/Layout.tsx
@@ -5,7 +5,7 @@ import { Header } from "./Header";
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div>
-      <div className="fixed inset-x-0 top-0">
+      <div className="fixed inset-x-0 top-0 z-50">
         <Header />
       </div>
       {children}
