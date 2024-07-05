e4e1ac94dd416ddf35fc45ad708b1c6b935070ba
Keita Ito <eguchi@eguchi.cc>
2024-07-05 14:42:49 +0900

    feat(www): プロフィールの作成

--- apps/www/src/app/(root)/page.tsx
+++ apps/www/src/app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import { Layout } from "@/layouts/Layout";
-import { Hero } from "./_components/Hero";
 import { SubHeader } from "@/layouts/SubHeader";
+import { Hero } from "./_components/Hero";
+import { Profile } from "./_sections/Profile";
+
+import styles from "./_page.module.css";
 
 export default function IndexPage() {
   return (
@@ -10,6 +13,11 @@ export default function IndexPage() {
         <SubHeader />
       </div>
       <div className="min-h-screen">a</div>
+      <section id="profile" className="bg-cyan-100">
+        <div className={styles.section}>
+          <Profile />
+        </div>
+      </section>
       <div className="min-h-screen">a</div>
       <div className="min-h-screen">a</div>
       <div className="min-h-screen">a</div>
--- apps/www/src/layouts/SubHeader.tsx
+++ apps/www/src/layouts/SubHeader.tsx
@@ -1,24 +1,22 @@
-import Link from "next/link";
-
 const items = [
-  { label: "HOME" },
-  { label: "WORKS" },
-  { label: "ABOUT" },
-  { label: "CONTACT" },
+  { label: "HOME", href: "#" },
+  { label: "Profile", href: "#profile" },
+  { label: "ABOUT", href: "#" },
+  { label: "CONTACT", href: "#" },
 ];
 
 export function SubHeader() {
   return (
     <div className="flex h-12 backdrop-blur">
       <ul className="grid w-full auto-cols-fr grid-flow-col divide-x divide-primary-50 border-x">
-        {items.map(({ label }) => (
+        {items.map(({ label, href }) => (
           <li key={label}>
-            <Link
-              href="#"
+            <a
+              href={href}
               className="flex h-full items-center justify-center bg-primary-100/70 hover:bg-primary-200/70"
             >
               {label}
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
