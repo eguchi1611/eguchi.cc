9156f647d2570e8c3f0b49d56eefd47a20062640
Keita Ito <eguchi@eguchi.cc>
2024-07-08 12:01:10 +0900

    chore(www): 不要なレイアウトを削除

--- apps/www/src/app/(root)/page.tsx
+++ apps/www/src/app/(root)/page.tsx
@@ -1,10 +1,5 @@
 import { Layout } from "@/layouts/Layout";
-import { SubHeader } from "@/layouts/SubHeader";
-import { Articles } from "./_components/Articles";
 import { Hero } from "./_components/Hero";
-import { Profile } from "./_components/Profile";
-
-import styles from "./_page.module.css";
 
 export default function IndexPage() {
   return (
@@ -12,19 +7,6 @@ export default function IndexPage() {
       <div id="top">
         <Hero />
       </div>
-      <div className="sticky top-16">
-        <SubHeader />
-      </div>
-      <section id="profile" className="bg-cyan-50">
-        <div className={styles.section}>
-          <Profile />
-        </div>
-      </section>
-      <section id="articles" className="bg-yellow-50">
-        <div className={styles.section}>
-          <Articles />
-        </div>
-      </section>
       <div className="min-h-screen">a</div>
       <div className="min-h-screen">a</div>
       <div className="min-h-screen">a</div>
--- apps/www/src/layouts/Layout.tsx
+++ apps/www/src/layouts/Layout.tsx
@@ -1,13 +1,9 @@
 import { PropsWithChildren } from "react";
 
-import { Header } from "./Header";
-
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div>
-      <div className="fixed inset-x-0 top-0 z-50">
-        <Header />
-      </div>
+      <div className="fixed inset-x-0 top-0 z-50">{/* <Header /> */}</div>
       {children}
     </div>
   );
