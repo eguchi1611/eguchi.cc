3c02b19b56fb22aba15ad5f5cbd48de48863a626
Keita Ito <eguchi@eguchi.cc>
2024-07-08 20:43:30 +0900

    fix(www): aboutmeのレイアウト構造を修正

--- apps/www/src/app/(root)/_components/Aboutme.tsx
+++ apps/www/src/app/(root)/_components/Aboutme.tsx
@@ -10,45 +10,47 @@ import icon from "../_assets/icon.png";
 
 export function Aboutme() {
   return (
-    <div className="grid h-full grid-cols-2 place-items-center">
-      <h2 className="col-span-2 text-center text-xl font-bold text-gray-900">
-        <a href="#aboutme">About me</a>
+    <div className="flex h-full flex-col">
+      <h2 className="mb-1 text-center text-xl font-bold text-gray-900">
+        <a href="#aboutme" className="block">
+          About me
+        </a>
       </h2>
-      <div>
+      <div className="grid flex-1 place-items-center gap-4 sm:grid-cols-2">
         <Image
           src={icon.src}
           alt="Keita Ito"
           width={500}
           height={500}
-          className="h-32 w-32 rounded-full"
+          className="mx-auto h-32 w-32 rounded-full"
         />
+        <ul className="grid grid-cols-1 gap-2">
+          <li className="flex gap-4">
+            <IconSignature />
+            <div>
+              伊藤啓太 (Keita Ito)
+              <br />
+              えぐち
+            </div>
+          </li>
+          <li className="flex gap-4">
+            <IconCake />
+            <div>2004年7月9日 (20歳)</div>
+          </li>
+          <li className="flex gap-4">
+            <IconMapPin />
+            <div>東京都調布市</div>
+          </li>
+          <li className="flex gap-4">
+            <IconSchool />
+            <div>
+              電気通信大学 情報理工学域
+              <br />
+              メディア情報学プログラム
+            </div>
+          </li>
+        </ul>
       </div>
-      <ul className="flex flex-col gap-2">
-        <li className="flex gap-4">
-          <IconSignature />
-          <div>
-            伊藤啓太 (Keita Ito)
-            <br />
-            えぐち
-          </div>
-        </li>
-        <li className="flex gap-4">
-          <IconCake />
-          <div>2004年7月9日 (20歳)</div>
-        </li>
-        <li className="flex gap-4">
-          <IconMapPin />
-          <div>東京都調布市</div>
-        </li>
-        <li className="flex gap-4">
-          <IconSchool />
-          <div>
-            電気通信大学 情報理工学域
-            <br />
-            メディア情報学プログラム
-          </div>
-        </li>
-      </ul>
     </div>
   );
 }
