577df92e5470fc66010b9edd2410ba56a1d948a9
Keita Ito <eguchi@eguchi.cc>
2024-07-10 00:14:30 +0900

    fix(www): heroの背景コンポーネントを分離

--- apps/www/src/app/(root)/_components/Hero.tsx
+++ apps/www/src/app/(root)/_components/Hero.tsx
@@ -3,31 +3,16 @@
 import { IconChevronsDown } from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRef } from "react";
-
-import { useOffsetTop } from "@/hooks/useOffsetTop";
-import { useWindowSize } from "@/hooks/useWindowSize";
-import { minmax } from "@/lib/utils";
 
 import styles from "./Hero.module.css";
+import { HeroBackground } from "./HeroBackground";
 
 export function Hero() {
-  const masterRef = useRef(null);
-  const { viewportTop } = useOffsetTop(masterRef);
-  const { windowSize } = useWindowSize();
-
-  // useMemo使わない方がパフォーマンスが良い
-  const scroll = calcRatio(viewportTop || undefined, windowSize?.height);
-
   return (
     <div
-      ref={masterRef}
       className={`relative grid min-h-96 place-content-center place-items-center bg-primary-50 bg-fixed ${styles["bg-pattern"]}`}
     >
-      <div
-        className="absolute inset-0 bg-primary-700 transition-none"
-        style={{ opacity: Math.min(scroll, 0.7) }}
-      />
+      <HeroBackground />
       <Link href="/" className="z-10">
         <Image
           alt=""
@@ -42,14 +27,4 @@ export function Hero() {
       </a>
     </div>
   );
-}
-
-/**
- * 画面の表示比率を計算
- * @param viewportTop 要素の位置
- * @param windowHeight ウィンドウの高さ
- * @returns 画面の表示比率
- */
-function calcRatio(viewportTop = 0, windowHeight = 1): number {
-  return minmax(0, 1 - (viewportTop + windowHeight) / windowHeight, 1);
 }
