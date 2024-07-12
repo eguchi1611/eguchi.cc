8df94f0d7cc9887483c286a34f9d99bac0eee012
Keita Ito <eguchi@eguchi.cc>
2024-07-13 08:37:03 +0900

    fix(www): some fix

--- apps/www/src/app/(root)/_components/Contact.tsx
+++ apps/www/src/app/(root)/_components/Contact.tsx
@@ -9,21 +9,32 @@ export function Contact() {
         </a>
       </h2>
       <ul className="grid flex-1 gap-4 lg:grid-cols-2 lg:place-items-center">
-        <li className="flex gap-2">
-          <div className="rounded-full bg-primary-500 p-2 text-white">
-            <IconMail />
-          </div>
-          <div className="flex items-center gap-1">
-            contact
-            <IconAt size={18} />
-            eguchi.cc
+        <li>
+          <div className="flex gap-2">
+            <div className="rounded-full bg-primary-500 p-2 text-white">
+              <IconMail />
+            </div>
+            <div className="flex items-center gap-1">
+              contact
+              <IconAt size={18} />
+              eguchi.cc
+            </div>
           </div>
         </li>
-        <li className="flex gap-2">
-          <div className="rounded-full bg-primary-500 p-2 text-white">
-            <IconBrandX />
-          </div>
-          <div className="flex items-center gap-1">@keita_ito_</div>
+        <li>
+          <a
+            href="https://x.com/keita_ito_"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group flex gap-2"
+          >
+            <div className="rounded-full bg-primary-500 p-2 text-white">
+              <IconBrandX />
+            </div>
+            <div className="flex items-center gap-1 group-hover:underline">
+              @keita_ito_
+            </div>
+          </a>
         </li>
       </ul>
     </div>
--- apps/www/src/app/(root)/_components/Environment.tsx
+++ apps/www/src/app/(root)/_components/Environment.tsx
@@ -7,6 +7,7 @@ export function Environment() {
         </a>
       </h2>
       {/* todo */}
+      編集中
     </div>
   );
 }
