1a2c0105b7c8ac5d95e61928e8479920e43a4973
Keita Ito <eguchi@eguchi.cc>
2024-07-19 17:23:46 +0900

    chore(www): 不要レイアウトの構成

--- apps/www/src/app/(root)/layout.tsx
+++ apps/www/src/app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/layouts/Header";
 import { Layout as ParentLayout } from "@/layouts/Layout";
 import { PropsWithChildren } from "react";
 
-export function Layout({ children }: PropsWithChildren) {
+export default function Layout({ children }: PropsWithChildren) {
   return (
     <ParentLayout>
       <div className="fixed inset-x-0 top-0 z-50">
--- apps/www/src/app/works/page.tsx
+++ apps/www/src/app/works/page.tsx
@@ -1,3 +1,32 @@
-export default function WorkIndexPage() {
-  return <div className="">Work Index</div>;
+import { getSlugs, readTextFile } from "@/lib/articles";
+import { compileMDX } from "next-mdx-remote/rsc";
+import Link from "next/link";
+import { join } from "path";
+
+const baseDir = join(process.cwd(), "_works");
+
+export default async function WorkIndexPage() {
+  const slugs = getSlugs(baseDir);
+  const articles = await Promise.all(
+    slugs.map(async (slug) => {
+      const source = await readTextFile(baseDir, slug);
+      const { content, frontmatter } = await compileMDX<{ title: string }>({
+        source,
+        options: {
+          parseFrontmatter: true,
+        },
+      });
+      return { content, frontmatter, slug };
+    }),
+  );
+
+  return (
+    <div className="">
+      {articles.map(({ slug, frontmatter }) => (
+        <div key={slug}>
+          <Link href={`/works/${slug}`}>{frontmatter.title || ""}</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
