6411d325403c54a56d23a1a56ebaf1a37132a329
Keita Ito <eguchi@eguchi.cc>
2024-07-23 06:36:38 +0900

    chore: スタイルベースの構成

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="ja">
-      <body>{children}</body>
+    <html lang="ja" className="scroll-smooth scroll-pt-16">
+      <body className="text-foreground bg-primary-50">{children}</body>
     </html>
   );
 }
