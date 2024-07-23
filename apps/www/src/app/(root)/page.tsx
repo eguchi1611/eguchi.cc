0c6fe3b7462240b883b2ad029b699833dc653a39
Keita Ito <eguchi@eguchi.cc>
2024-07-23 09:18:08 +0900

    feat: DesktopSidebarを作成

--- apps/www/src/app/(root)/page.tsx
+++ apps/www/src/app/(root)/page.tsx
@@ -1,3 +1,18 @@
+import { DesktopSidebar } from "../_components/DesktopSidebar";
+
 export default function IndexPage() {
-  return <div className="text-center">Index Page</div>;
+  return (
+    <div className="bg-main">
+      <div className="container mx-auto flex">
+        <div className="sticky top-0 h-screen w-56 border-r bg-white">
+          <DesktopSidebar />
+        </div>
+        <div className="flex-1 bg-teal-50">
+          <div className="min-h-screen">Content 1</div>
+          <div className="min-h-screen">Content 2</div>
+          <div className="min-h-screen">Content 3</div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ apps/www/src/app/_components/DesktopSidebar.tsx
@@ -0,0 +1,13 @@
+export function DesktopSidebar() {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <img
+        src="/logo.svg"
+        alt="logo"
+        width="96.48"
+        height="21.26"
+        className="h-auto w-full"
+      />
+    </div>
+  );
+}
