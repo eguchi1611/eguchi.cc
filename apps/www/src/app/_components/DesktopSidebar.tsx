22b05d682b1fda65b390e2e75537fa9899b1101d
Keita Ito <eguchi@eguchi.cc>
2024-07-23 22:05:35 +0900

    chore: サイドバーアイコンの実装

--- apps/www/src/app/_components/DesktopSidebar.tsx
+++ apps/www/src/app/_components/DesktopSidebar.tsx
@@ -1,3 +1,11 @@
+const items = [
+  { src: "/twitter.svg", alt: "twitter", width: 128, height: 128 },
+  { src: "/bluesky.svg", alt: "bluesky", width: 682.667, height: 682.667 },
+  { src: "/qiita.svg", alt: "qiita", width: 400, height: 400 },
+  { src: "/zenn.svg", alt: "zenn", width: 88.3, height: 88.3 },
+  { src: "/github.svg", alt: "github", width: 128, height: 128 },
+] as const;
+
 export function DesktopSidebar() {
   return (
     <div className="flex flex-col gap-4 p-4">
@@ -8,6 +16,13 @@ export function DesktopSidebar() {
         height="21.26"
         className="h-auto w-full"
       />
+      <div className="flex justify-between gap-1">
+        {items.map(({ alt, ...props }) => (
+          <a key={alt} href="#" target="_blank" rel="noopener noreferrer">
+            <img {...props} alt={alt} className="h-8 w-auto" />
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
