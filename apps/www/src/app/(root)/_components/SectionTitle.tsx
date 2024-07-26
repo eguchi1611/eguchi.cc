c459f0e398c4dc70d1dec5dc1ec485870b5b1543
Keita Ito <eguchi@eguchi.cc>
2024-07-26 18:40:45 +0900

    fix: SectionTitleのアニメーションを変更

--- apps/www/src/app/(root)/_components/SectionTitle.tsx
+++ apps/www/src/app/(root)/_components/SectionTitle.tsx
@@ -9,7 +9,7 @@ export function SectionTitle(props: Props) {
   return (
     <div className="text-xl font-bold" id={props.href}>
       <a href={`#${props.href}`} className="group flex items-center gap-1">
-        <IconSectionSign className="group-hover:animate-spin" />
+        <IconSectionSign className="group-hover:animate-ping" />
         {props.label}
       </a>
     </div>
