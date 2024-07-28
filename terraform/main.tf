cf30d912fbc12febb74fa84082e5a34e02dbf0ac
Keita Ito <eguchi@eguchi.cc>
2024-07-28 17:26:06 +0900

    feat(infra): ドメインレコードの登録

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,7 +97,62 @@ resource "aws_cloudfront_distribution" "www" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = var.acm_certificate_arn
+    acm_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
     ssl_support_method  = "sni-only"
   }
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+  provider          = aws.virginia
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_zone" "primary" {
+  name = var.acm_zone_domain
+}
+
+resource "aws_route53_record" "cert" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+  zone_id = aws_route53_zone.primary.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider                = aws.virginia
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert : record.fqdn]
+}
+
+resource "aws_route53_record" "www" {
+  type    = "A"
+  name    = var.domain_name
+  zone_id = aws_route53_zone.primary.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.www.domain_name
+    zone_id                = aws_cloudfront_distribution.www.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_s3_object" "sample" {
+  bucket       = aws_s3_bucket.www.bucket
+  key          = "index.html"
+  content      = "hello terraform\n"
+  content_type = "text/html"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,6 +7,6 @@ variable "domain_name" {
   type = string
 }
 
-variable "acm_certificate_arn" {
+variable "acm_zone_domain" {
   type = string
 }
