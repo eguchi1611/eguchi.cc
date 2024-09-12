1dd7bc22cdf6bf36f44593e9410b831c68b6b4da
Keita Ito <eguchi@eguchi.cc>
2024-09-12 13:36:52 +0900

    chore: プロジェクトの削除

--- apps/www/src/app/works/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "@/ui/Layout";
-
-export default function WorksPage() {
-  return (
-    <Layout>
-      <h1>Works</h1>
-    </Layout>
-  );
-}
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_s3_bucket_name" {
-  value = aws_s3_bucket.www.bucket
-}
-
-output "aws_cloudfront_distribution_domain_name" {
-  value = aws_cloudfront_distribution.www.domain_name
-}
