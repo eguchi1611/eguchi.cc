1c4ea95c9cffc2fd893e081a92257d9efdf83640
Keita Ito <eguchi@eguchi.cc>
2024-09-27 13:24:50 +0900

    feat: プロフィールカードの作成

---
+++ website/src/app/(index)/_components/cards/card-base.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+
+interface CardBaseProps {
+  title: string;
+}
+
+function CardBase(props: PropsWithChildren<CardBaseProps>) {
+  return (
+    <div className="rounded border p-4">
+      <h2 className="text-xl font-bold">{props.title}</h2>
+      <div className="mt-2">{props.children}</div>
+    </div>
+  );
+}
+
+export { CardBase };
--- website/src/app/(index)/page.tsx
+++ website/src/app/(index)/page.tsx
@@ -1,14 +1,9 @@
-import ProfileCard from "./_components/cards/profile-card";
+import { ProfileCard } from "./_components/cards/profile-card";
 
 function IndexPage() {
   return (
-    <div className="flex">
-      <div>sidebar</div>
-      <div className="container mx-auto">
-        <h1 className="text-teal-500">Index Page</h1>
-        <p>v1.0.9</p>
-        <ProfileCard />
-      </div>
+    <div className="container mx-auto px-2">
+      <ProfileCard />
     </div>
   );
 }
