62b8553c348847549e2c9abaaff45cd0d19b4e8e
Keita Ito <eguchi@eguchi.cc>
2024-10-06 14:41:27 +0900

    feat: サイドバーのデザイン

--- website/src/components/layout.tsx
+++ website/src/components/layout.tsx
@@ -5,11 +5,11 @@ import { Sidebar } from "./sidebar";
 function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <div className="mx-auto flex max-w-5xl items-start px-2">
-        <div className="sticky left-0 top-0 mr-4">
+      <div className="mx-auto flex max-w-5xl items-start px-4">
+        <div className="sticky left-0 top-0 mr-4 pt-4">
           <Sidebar />
         </div>
-        <div className="pt-4">{children}</div>
+        <div className="min-h-screen pt-4">{children}</div>
       </div>
       <Footer />
     </>
--- website/src/components/sidebar.tsx
+++ website/src/components/sidebar.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const items = [
+  { label: "Home", href: "/" },
+  { label: "Works", href: "/works" },
+];
+
 function Sidebar() {
   return (
-    <div className="w-48 bg-white">
-      <h1 className="py-4 text-center text-3xl">Sidebar</h1>
+    <div className="w-64 divide-y rounded border bg-white">
+      <Link href="/">
+        <h1 className="py-4">
+          <Image alt="logo" src="/logo.svg" width={317} height={39} className="mx-auto block h-6 w-auto" priority />
+          <span className="sr-only">keitaito.net</span>
+        </h1>
+      </Link>
+      <ul>
+        {items.map((item) => (
+          <li key={item.label}>
+            <Link className="block py-2 text-center" href={item.href}>
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
