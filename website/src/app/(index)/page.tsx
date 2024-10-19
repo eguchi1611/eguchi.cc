23eab1c13e59e920b1d482c9a2ef70e759232127
Keita Ito <eguchi@eguchi.cc>
2024-10-19 16:21:44 +0900

    chore: next-uiのインストール

--- website/src/app/(index)/page.tsx
+++ website/src/app/(index)/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@nextui-org/react";
+
 export default function IndexPage() {
-  return <div className="underline">index page</div>;
+  return (
+    <div className="text-center">
+      <h1>Index Page</h1>
+      <p>This is the index page.</p>
+      <Button color="primary">Click me</Button>
+    </div>
+  );
 }
