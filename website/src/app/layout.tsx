80724fba0054fb4d4d118cb85950a21391ce5390
Keita Ito <eguchi@eguchi.cc>
2024-10-21 03:36:50 +0900

    chore: SEO対策

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,8 +1,20 @@
+import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
 import { BaseLayout } from "./_components/base-layout";
 import { Providers } from "./_components/providers";
 
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | keitaito.net",
+    default: "keitaito.net",
+  },
+  description: "Developer / Engineer",
+  alternates: {
+    canonical: "https://keitaito.net",
+  },
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
