ac2f4bbb36a542a9adcd1d458201ea8ab4da63aa
Keita Ito <eguchi@eguchi.cc>
2024-10-21 02:55:22 +0900

    refactor: フッターのコンポーネントを追加

---
+++ website/src/app/_components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="mt-4 bg-primary-50 py-8">
+      <div className="mx-auto max-w-7xl px-6 text-center">(c) 2021 Keita Ito</div>
+    </footer>
+  );
+}
