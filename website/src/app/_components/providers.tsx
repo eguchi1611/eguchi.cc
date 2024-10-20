1ad6e6fe9c1de4f83207fbec411fe0f409d28f91
Keita Ito <eguchi@eguchi.cc>
2024-10-21 02:16:47 +0900

    chore: NextUIProviderの構成

--- website/src/app/_components/providers.tsx
+++ website/src/app/_components/providers.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import type { PropsWithChildren } from "react";
 
 export function Providers({ children }: PropsWithChildren) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  const router = useRouter();
+  return (
+    <NextUIProvider navigate={router.push} locale="ja-JP">
+      {children}
+    </NextUIProvider>
+  );
 }
